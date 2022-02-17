c6bc30b03a6127e72f34ecc765c28aa169052943
sllngshot <slingshotutube@gmail.com>
2022-02-17 15:04:53 +1000

    added some testing units

--- vstris.py
+++ vstris.py
@@ -1,20 +1,12 @@
+import backdrop
+
 import pygame
-from pygame.locals import *
 import os
+from PIL import Image
+
+from pygame.locals import *
 from typing import List, Tuple
 
-class Animated:
-    """
-    Wrapper class to hold animated elements of 
-    Vstris game.
-    """
-    def __init__(self, frames: List[pygame.Surface], speed: float):
-        self.frames = frames
-        self.current_frame = 0
-        self.is_running = False
-        self.speed
-        
-    
 
 class Vstris:
     """
@@ -22,37 +14,61 @@ class Vstris:
     gui rendering, hosting the main surface window 
     pygame creates
     """
-    
-    # General use variables
+    # CONSTANTS -------------------------------- #
     WIDTH, HEIGHT = 0, 0
     FPS = 144
+    if not pygame.font.get_init(): pygame.font.init()
+    FONT = pygame.font.SysFont("Fira Code", 60)
 
-    #Colors
+    # COLOURS ---------------------------------- #
     WHITE = (255, 255, 255) 
     BLACK = (0, 0, 0)
     FOREGROUND = (85, 81, 81)
     BACKGROUND = (30, 25, 25)
 
     def __init__(self, window: pygame.Surface, dimensions: Tuple[int, int]):
+        """
+        Initialize Vstris game
+        """
+        # SET DEFAULTS ------------------------- #
         self.win = window
         self.running = True
         self.fullscreen = True
-        self.mode = "main_menu"
+        self.mode = "load"
+        self.scale = 1
         Vstris.WIDTH = dimensions[0]
         Vstris.HEIGHT = dimensions[1]
 
-        # IMAGE ASSETS ------------------ #
-        self.background = pygame.image.load(os.path.join("assets", "backdrop", "backdrop.png")).convert()
-        self.board = pygame.image.load(os.path.join("assets", "board", "board.png")).convert()
+        # LOAD ASSETS -------------------------- #
+        self.load_assets(self.scale)
+
+        #
+
+    def load_assets(self, scale: float):
+        # LOAD TEXTS ------------------- #
+        self.title = Vstris.FONT.render("Vstris", True, Vstris.WHITE,)
+
+        # LOAD IMAGES ------------------ #
+        self.board_img = pygame.image.load(os.path.join("assets", "Board", "board.png")).convert()
+
+        self.block_1 = pygame.image.load(os.path.join("assets", "Tetriminos", "Purple.png")).convert()
+        self.block_2 = pygame.image.load(os.path.join("assets", "Tetriminos", "yellow.png")).convert()
+        self.block_3 = pygame.image.load(os.path.join("assets", "Tetriminos", "red.png")).convert()
+        self.block_4 = pygame.image.load(os.path.join("assets", "Tetriminos", "green.png")).convert()
+        self.block_5 = pygame.image.load(os.path.join("assets", "Tetriminos", "orange.png")).convert()
+        self.block_6 = pygame.image.load(os.path.join("assets", "Tetriminos", "dark_blue.png")).convert()
+        self.block_7 = pygame.image.load(os.path.join("assets", "Tetriminos", "blue.png")).convert()
         
-        # Blocks
-        self.block_1 = pygame.image.load(os.path.join("assets", "backdrop", "backdrop.png")).convert()
+        self.backdrop = pygame.image.load(os.path.join("assets", "Backdrop", "Backdrop.png")).convert()
+        
+        
 
-
+        # SCALE IMAGE ------------------- #
+        self.board = pygame.transform.scale(self.board_img, (self.board_img.get_size()))
 
     def run(self):
         """
-        Main game loop. 
+        Main game loop
         
         All game logic is processed here, and all drawing is done
         at the end of each frame
@@ -71,20 +87,27 @@ class Vstris:
 
                 if event.type == pygame.KEYDOWN:
                     if event.key == K_F11:
-                        self.fullscreen = not self.fullscreen
-                        if self.fullscreen:
-                            self.win = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-                        else:
-                            self.win = pygame.display.set_mode((1000, 800))
+                        self.toggle_fullscreen()
+                    elif event.key == K_ESCAPE:
+                        self.mode == "pause"
+                        
 
             # CLEAR FRAME ----------------------- #
-            self.win.fill(Vstris.BACKGROUND)
-            #self.win.blit(self.background, (0, 0))
-            self.win.blit(self.board, (350, 100))
+            self.win.blit(self.backdrop, (0,0))
+
 
             # PICK STATE AND DRAW --------------- #
-            if self.mode == "main_menu":
-                pass
+            if self.mode == "load":
+                self.mode = "main_menu"
+                
+                self.draw_title()
+                self.draw_menu()
+                self.draw_version()
+
+            elif self.mode == "main_menu":
+                self.draw_title()
+                self.draw_menu()
+                self.draw_version()
 
             elif self.mode == "settings":
                 pass
@@ -92,10 +115,39 @@ class Vstris:
             elif self.mode == "singeplayer":
                 pass
 
+            elif self.mode == "pause":
+                self.win.fill(Vstris.WHITE)
 
             # UPDATE SCREEN --------------------- #
+            self.win.blit(self.board, (350, 100))
             pygame.display.update()
 
+
+    def draw_title(self):
+        self.win.blit(self.title, (100, 100))
+
+    def draw_menu(self):
+        pass
+
+    def draw_version(self):
+        pass
+
+
+
+
+    def toggle_fullscreen(self):
+
+        self.fullscreen = not self.fullscreen
+        if self.fullscreen: 
+            self.win = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        else: 
+            self.win = pygame.display.set_mode((1920, 1000))
+
+        Vstris.WIDTH, Vstris.HEIGHT = self.win.get_size()
+        self.load_assets(self.scale)
+
+
+    
 
 if __name__ == "__main__":
     """
